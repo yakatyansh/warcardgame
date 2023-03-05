47ba9aa2f087ff3a7b8bfe73beee220b560cc6f9
yakatyansh <ykpetrolhead08@outlook.com>
2023-03-06 05:11:25 +0530

    Initial Commit

---
+++ war card game/war_card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_card_gameApp.swift
+//  war card game
+//
+//  Created by Yash Katyan on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct war_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
