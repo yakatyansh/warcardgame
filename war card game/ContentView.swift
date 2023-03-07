c60163fea183d6b42850375f0dc3dbbba7924fff
yakatyansh <ykpetrolhead08@outlook.com>
2023-03-07 18:29:58 +0530

    add ui

--- war card game/ContentView.swift
+++ war card game/ContentView.swift
@@ -9,18 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Image("background-cloth")
+            VStack{
+                Image("logo")
+                    .padding()
+                HStack{
+                    Image("card3")
+                        .padding()
+                    Image("card3")
+                    
+                }
+                Image("button")
+                    .padding()
+                 HStack{
+                    VStack{
+                        Text("Player")
+                            .padding()
+                            .foregroundColor(.white)
+                        Text("0")
+                            .foregroundColor(.white)
+                    }
+                    VStack{
+                        Text("CPU")
+                            .padding().foregroundColor(.white)
+                        Text("0")
+                            .foregroundColor(.white)
+                    }
+                }
+            }
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
